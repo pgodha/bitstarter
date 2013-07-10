3c3a6f8d4119dae2672eda7dca1c59178d495a2b
Ubuntu <ubuntu@ip-172-31-23-57.us-west-2.compute.internal>
2013-07-10 06:36:26 +0000

    Updated web.js to print content of index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
+//var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  
+	var buffer = fs.readFileSync(index.html);
+	console.log("index.html: "+buffer.toString());
+	response.send(buffer.toStringi('utf-8'));
+//	response.send('hello');
 });
 
 var port = process.env.PORT || 5000;
