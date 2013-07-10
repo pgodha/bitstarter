a14df939d88842713d505a26c54735a5ca37cb0b
Ubuntu <ubuntu@ip-172-31-23-57.us-west-2.compute.internal>
2013-07-10 06:40:37 +0000

    Updated web.js to print content of index.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   
-	var buffer = fs.readFileSync(index.html);
+	var buffer = fs.readFileSync('index.html');
 	console.log("index.html: "+buffer.toString());
 	response.send(buffer.toStringi('utf-8'));
 //	response.send('hello');
